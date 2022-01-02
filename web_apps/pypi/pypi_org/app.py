08efd4597f48261f0a03dda0c5548109d2f9801b
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-01-03 00:15:02 +0100

    Add jinja2 dependency Add flask render template example with fake get packages func

--- web_apps/pypi/pypi_org/app.py
+++ web_apps/pypi/pypi_org/app.py
@@ -4,9 +4,17 @@ import flask
 
 app = flask.Flask(__name__)
 
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.0a'},
+        {'name': 'jinja2', 'version': '1.3.0b'},
+        {'name': 'passlib', 'version': '1.4.0c'},
+    ]
+
 @app.route('/')
 def index():
-    return 'Hi bro!'
+    test_packages = get_latest_packages()
+    return flask.render_template('index.html', packages=test_packages)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
