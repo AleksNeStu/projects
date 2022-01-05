b928555fe6af1f7e0907494d3ff46c7e2ffe0a05
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-01-05 22:54:04 +0100

    Add home page html layout structure

--- web_apps/pypi/pypi_org/services/package_service.py
+++ web_apps/pypi/pypi_org/services/package_service.py
@@ -1,6 +1,6 @@
 def get_latest_packages():
     return [
-        {'name': 'flask', 'version': '1.2.0a'},
-        {'name': 'jinja2', 'version': '1.3.0b'},
-        {'name': 'passlib', 'version': '1.4.0c'},
+        {'name': 'flask', 'version': '1.2.0'},
+        {'name': 'jinja2', 'version': '1.3.0'},
+        {'name': 'passlib', 'version': '1.4.0'},
     ]
