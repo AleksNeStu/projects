0f3c5db452494de8b0d7fd5d7eb697163146cd56
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-01-05 13:19:43 +0100

    Change env debug=0. Adjust package_views.py. Add details.html.

--- web_apps/pypi/pypi_org/views/package_views.py
+++ web_apps/pypi/pypi_org/views/package_views.py
@@ -8,6 +8,11 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 @blueprint.route('/project/<package_name>')
 # @response(template_file='package/details.html')
-def package_details(package_name):
+def package_details(package_name: str):
     return "Package details for {}".format(package_name)
-    # return flask.render_template('home/index.html', packages=TEST_PACKAGES)
+
+
+@blueprint.route('/<int:rank>')
+def popular_package(rank: int):
+    # print(type(rank), rank)
+    return "The details for {}th most popular package".format(rank)
