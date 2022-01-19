cab22663c39ace7602b39dd091bad3f9c329bb23
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-01-19 18:36:56 +0100

    Add get package desc from DB to home page.

--- web_apps/pypi/pypi_org/views/home_views.py
+++ web_apps/pypi/pypi_org/views/home_views.py
@@ -10,17 +10,19 @@ blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 @response(template_file='home/index.html')
 def index():
     packages = package_service.get_packages()
+    latest_packages = package_service.get_latest_packages(3, packages)
     # releases ordered by relationship in data.models.package.Package
-    latest_packages = [
-        {'name': p.id, 'version': p.releases[0].version_text}
-        for p in package_service.get_latest_packages(3, packages)
-    ]
+    latest_packages_map = [{
+        'name': p.id,
+        'latest_version': p.releases[0].version_text,
+        'summary': p.summary,
+    } for p in latest_packages]
 
     releases = release_service.get_releases()
     users = user_service.get_users()
 
     return {
-        'latest_packages': latest_packages,
+        'latest_packages': latest_packages_map,
         'package_count': packages.count(),
         'release_count': releases.count(),
         'user_count': users.count(),
