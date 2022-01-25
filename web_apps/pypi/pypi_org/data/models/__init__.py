9074c442fff735cf7cd066930255f957493807e1
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-01-25 18:10:36 +0100

    Set import data models manually

--- web_apps/pypi/pypi_org/data/models/__init__.py
+++ web_apps/pypi/pypi_org/data/models/__init__.py
@@ -1,5 +1,16 @@
 """ORM models."""
-from utils import py as py_utils
+import data.models.audit
+import data.models.downloads
+import data.models.languages
+import data.models.licenses
+import data.models.maintainers
+import data.models.package
+import data.models.releases
+import data.models.users
 
 
-py_utils.import_modules(__file__, __name__, w_classes=True)
+# import data.models.all pypi_org/data/models
+# from utils import data.models.py as py_utils
+#
+#
+# py_utils.import_modules(__file__, __name__, w_classes=True)
