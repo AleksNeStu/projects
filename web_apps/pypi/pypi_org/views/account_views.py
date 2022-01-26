dbc6d56e9ba0ab7f8fae8a7ea7a368918945c499
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-01-26 21:43:58 +0100

    Add views.account_views.register_post desc. Adjust register.html

--- web_apps/pypi/pypi_org/views/account_views.py
+++ web_apps/pypi/pypi_org/views/account_views.py
@@ -1,7 +1,7 @@
 import flask
+from werkzeug.datastructures import ImmutableMultiDict
 
 from infra.view_modifiers import response
-
 
 blueprint = flask.Blueprint('account', __name__, template_folder='templates')
 
@@ -23,6 +23,17 @@ def register_get():
 @blueprint.route('/account/register', methods=['POST'])
 @response(template_file='account/register.html')
 def register_post():
+    r = flask.request
+
+    # <input type="text" class="form-control" placeholder=" Email"
+    #  name="email" value="">
+    # key: name attribute of input tag
+    # value: post data from end user (filed via UI)
+    post_form: ImmutableMultiDict = r.form
+    # ImmutableMultiDict([('name', 'sd'), ('email', 'sd'), ('password', 'sd')])
+    post_form_dict: dict = post_form.to_dict()
+    # {'name': '_n', 'email': '_e', 'password': '_p'}
+
     return {}
 
 
