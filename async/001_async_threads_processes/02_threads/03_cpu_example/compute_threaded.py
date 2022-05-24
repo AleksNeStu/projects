070af4d2136fc9ddec3936083ebe6b61ca8b714c
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-05-24 17:33:30 +0200

    async/001_async_threads_processes/02_threads/03_cpu_example

---
+++ async/001_async_threads_processes/02_threads/03_cpu_example/compute_threaded.py
@@ -0,0 +1,42 @@
+# GIL problem
+import datetime
+import math
+from threading import Thread
+import multiprocessing
+
+CONST = 300_000_00
+
+def main():
+    do_math(1)
+
+    t0 = datetime.datetime.now()
+
+    # do_math(num=30000000)
+    print(f"Doing math on {multiprocessing.cpu_count():,} processors.")
+
+    processor_count = multiprocessing.cpu_count()
+    threads = []
+    for n in range(1, processor_count + 1):
+        threads.append(Thread(target=do_math,
+                              args=(CONST * (n - 1) / processor_count,
+                                    CONST * n / processor_count),
+                              daemon=True)
+                       )
+
+    [t.start() for t in threads]
+    [t.join() for t in threads]
+
+    dt = datetime.datetime.now() - t0
+    print(f"Done in {dt.total_seconds():,.2f} sec.")
+
+
+def do_math(start=0, num=10):
+    pos = start
+    k_sq = 1000 * 1000
+    while pos < num:
+        pos += 1
+        math.sqrt((pos - k_sq) * (pos - k_sq))
+
+
+if __name__ == '__main__':
+    main()
