0e815bf28952abeb64ba4fca132c6693d7e9e2de
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-09-05 18:30:10 +0200

    Make home urls part atomic

---
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views as home_views
+
+urlpatterns = [
+    path('home/', home_views.home),
+]
--- frameworks/django/003-become_a_django_developer/01-django_essential_training/smartnotes/urls.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/smartnotes/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from home import views as home_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_views.home),
+    path('', include('home.urls'))
 ]
