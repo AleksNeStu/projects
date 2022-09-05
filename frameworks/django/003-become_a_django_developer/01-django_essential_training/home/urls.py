c6a59b37b780b1c09505784853aae049f4c62477
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-09-05 19:27:53 +0200

    Add auth check page and route with redirection to admin page

--- frameworks/django/003-become_a_django_developer/01-django_essential_training/home/urls.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/home/urls.py
@@ -4,4 +4,5 @@ from . import views as home_views
 
 urlpatterns = [
     path('home/', home_views.home),
+    path('auth/', home_views.auth),
 ]
--- frameworks/django/003-become_a_django_developer/01-django_essential_training/home/views.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/home/views.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
     # Django uses template frm to render it
+    # from django.http import HttpResponse
     # return HttpResponse('Home response')
 
     # {} info from view to template
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def auth(request):
+    return render(request, 'home/auth.html', {})
