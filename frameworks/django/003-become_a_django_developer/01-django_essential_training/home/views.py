d452039e159ed4584f7ee6004817d67a20f45b07
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-09-05 17:39:17 +0200

    Add MTV example for Dj

--- frameworks/django/003-become_a_django_developer/01-django_essential_training/home/views.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/home/views.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home response')
+    # Django uses template frm to render it
+    # return HttpResponse('Home response')
+
+    # {} info from view to template
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
