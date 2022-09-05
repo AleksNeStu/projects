cb8d99a40a13aff9bccb1c832a360430d8952ad4
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-09-05 16:15:47 +0200

    Add home app. Move dj sec env var to file.

--- frameworks/django/003-become_a_django_developer/01-django_essential_training/smartnotes/settings.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/smartnotes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vl10#b*9$ror0g2&==v1#^uw-7-d$cp6t*cae+m805@x3&-#r&'
+SECRET_KEY = os.environ.get('SECRET_KEY_DJ_001', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'home',
 ]
 
 MIDDLEWARE = [
