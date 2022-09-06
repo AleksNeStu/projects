c7c139cf24d5e39b9242309adf87e728f6b1aaae
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-09-06 17:32:07 +0200

    Add view(model) -> template example

--- frameworks/django/003-become_a_django_developer/01-django_essential_training/notes/models.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/notes/models.py
@@ -7,3 +7,10 @@ class Notes(models.Model):
     title = models.CharField(max_length=255, blank=True)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+    def __repr__(self):
+        return self.title
--- frameworks/django/003-become_a_django_developer/01-django_essential_training/notes/views.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/notes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from .models import Notes
+
+def all_notes(request):
+    all_notes = Notes.objects.all()
+    # all_notes_t = [t.title for t in all_notes]
+    all_notes_t = Notes.objects.values('title')
+    return render(request, 'notes/notes.html', {'notes': all_notes, 'all_notes': all_notes_t})
