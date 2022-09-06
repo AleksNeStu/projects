75b37f0c01a6c13f411e3e1c32d8a8718165bb03
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-09-06 15:44:48 +0200

    Add notes table to admin module

--- frameworks/django/003-become_a_django_developer/01-django_essential_training/notes/admin.py
+++ frameworks/django/003-become_a_django_developer/01-django_essential_training/notes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+import notes
+from . import models
 # Register your models here.
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title',) # display http://localhost:8000/admin/notes/notes/
+
+
+admin.site.register(models.Notes, NotesAdmin)
