acd44efddf88433f4604ea4c87eab285ac60444b
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-11-24 12:07:20 +0100

    Add rest of todo and readme files

--- scripts/python/_os.py
+++ scripts/python/_os.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""Foobar.py: Description of what foobar does."""
-
-__author__ = "AleksNeStu"
-__maintainer__ = "AleksNeStu"
-__copyright__ = "©AleksNeStu"
-__license__ = "The GNU General Public License v3.0"
-__version__ = "0.0.1"
-__contact__ = "https://github.com/AleksNeStu"
-__status__ = "Prototype"
-
 import os
 
 
