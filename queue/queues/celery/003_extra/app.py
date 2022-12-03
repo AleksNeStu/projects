74282fce390466d4964b2b28a30bd01925f81d79
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2022-12-03 18:02:51 +0100

    Update deps

--- queue/queues/celery/003_extra/app.py
+++
@@ -1,7 +0,0 @@
-# Choosing and installing a message transport (broker).
-#
-# Installing Celery and creating your first task.
-#
-# Starting the worker and calling tasks.
-#
-# Keeping track of tasks as they transition through different states, and inspecting return values.
