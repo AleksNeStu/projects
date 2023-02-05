30be96b91d1cc4e8eb07eab05113784ed9c4149c
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2023-02-05 16:53:19 +0100

    Update scripts.helpers.001_get_youtube_urls

--- scripts/helpers/001_get_youtube_urls/main.py
+++ scripts/helpers/001_get_youtube_urls/main.py
@@ -1,22 +1,35 @@
 import scrapetube
 
 CHANNEL_ID = "CHANNEL-ID"
-CHANNEL_URL = "CHANNEL_URL"
+CHANNEL_URL = "https://www.youtube.com/@user"
 PLAYLIST_ID = "PLAYLIST_ID"
 
-# Get all videos for a channel
-chanel_videos = list(scrapetube.get_channel(channel_id=CHANNEL_ID, channel_url=CHANNEL_URL))
-chanel_videos_id = [c_video['videoId'] for c_video in chanel_videos]
-for c_video in chanel_videos_id:
-    # print(f"https://youtu.be/{c_video}", sep="\n")
-    #https://www.youtube.com/?feature=youtu.be
-    print(f"https://www.youtube.com/watch?v={c_video}", sep="\n")
+def get_from_channel(channel_url=CHANNEL_URL):
+    # Get all videos for a channel
+    chanel_videos = list(scrapetube.get_channel(channel_url=channel_url))
+    chanel_videos_id = [c_video['videoId'] for c_video in chanel_videos]
+
+    video_urls = []
+    for c_video in chanel_videos_id:
+        # print(f"https://youtu.be/{c_video}", sep="\n")
+        #https://www.youtube.com/?feature=youtu.be
+        # print(f"https://www.youtube.com/watch?v={c_video}", sep="\n")
+        video_urls.append(f"https://www.youtube.com/watch?v={c_video}")
+    return video_urls
 
 
-# Get all videos for a playlist
-playlist_videos = list(scrapetube.get_playlist(playlist_id=PLAYLIST_ID))
-playlist_videos_id = [p_video['videoId'] for p_video in playlist_videos]
+def get_from_playlist(playlist_id=PLAYLIST_ID):
+    # Get all videos for a playlist
+    playlist_videos = list(scrapetube.get_playlist(playlist_id=playlist_id))
+    playlist_videos_id = [p_video['videoId'] for p_video in playlist_videos]
+    return playlist_videos_id
 
-# Make a search
-found_videos = list(scrapetube.get_search("python"))
-found_videos_id = [f_video['videoId'] for f_video in found_videos]
+def search(query: str = 'python'):
+    # Make a search
+    found_videos = list(scrapetube.get_search(query))
+    found_videos_id = [f_video['videoId'] for f_video in found_videos]
+
+if __name__ == '__main__':
+    video_urls = get_from_channel()
+    with open('r.txt', 'w') as f:
+        f.write('\n'.join(video_urls))
