1bef35ac7ebe2291b3e764a571dedad82ed2cfca
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2023-11-02 21:44:31 +0100

    Add env.py

--- common/env.py
+++ common/env.py
@@ -4,4 +4,6 @@ import dotenv
 dotenv.load_dotenv()  # equal to dotenv.load_dotenv("./.env")
 dotenv.load_dotenv("/sec/sec.env")
 
-DB_USER_NAME = os.environ.get("DB_PASS")
+DB_USER_NAME = os.environ.get("DB_USER_NAME")
+DB_USER_PASSWORD = os.environ.get("DB_USER_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
